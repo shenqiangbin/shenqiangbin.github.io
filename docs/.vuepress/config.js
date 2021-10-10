2f70ed18b28896573afc11d2f0c11c7474c28df1
shenqiangbin <1348907384@qq.com>
2021-10-10 23:31:06 +0800

    设置导航和首页

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+ module.exports = {
+     lang: 'zh-CN',
+     title: 'SQBER',
+     description: '个人站点',
+     themeConfig: {
+         logo: '/imgs/logo.png',
+         navbar: [
+            // NavbarItem
+            {
+              text: 'Java',
+              link: '/foo/',
+            },
+            // NavbarGroup
+            {
+              text: '大数据',
+              collapsable: false,
+              children: [
+                {
+                    text: 'Hadoop',
+                    link: '/bigdata/flume/intro.md'
+                },
+                {
+                    text: 'Hive',
+                    link: '/bigdata/flume/intro.md'
+                },
+                {
+                    text: 'HBase',
+                    link: '/bigdata/flume/intro.md'
+                },
+                {
+                    text: '数据采集',
+                    children:[
+                        {
+                            text: 'flume',
+                            link: '/bigdata/flume/intro.md'
+                        }
+                    ]
+                }
+            
+              ],
+            },
+            {
+                text: '关于我',
+                link: '/foo/',
+            }
+          ],
+     }
+ }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,28 @@
+---
+home: true
+title: Home
+heroImage: /imgs/logo.png
+# actions:
+#   - text: Get Started
+#     link: /guide/getting-started.html
+#     type: primary
+#   - text: Introduction
+#     link: /guide/
+#     type: secondary
+# features:
+#   - title: Simplicity First
+#     details: Minimal setup with markdown-centered project structure helps you focus on writing.
+#   - title: Vue-Powered
+#     details: Enjoy the dev experience of Vue, use Vue components in markdown, and develop custom themes with Vue.
+#   - title: Performant
+#     details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+#   - title: Themes
+#     details: Providing a default theme out of the box. You can also choose a community theme or create your own one.
+#   - title: Plugins
+#     details: Flexible plugin API, allowing plugins to provide lots of plug-and-play features for your site. 
+#   - title: Bundlers
+#     details: Both Webpack and Vite are supported. Choose the one you like!
+footer: Powered by VuePress | Copyright © 2018-present SQBER
+---
+
 # Hello baby.
