4ba79d587ef65ae41ea148354a8fab5234658c6f
shenqiangbin <1348907384@qq.com>
2022-01-13 23:15:11 +0800

    补充登录相关内容

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -158,7 +158,7 @@ const sidebar = {
       path: "/tech/myfunction",
       prefix: "myfunction/",
       collapsable: false,
-      children: ["unifiedReturnObject","unifiedException","validation","database","uploadFile","importData","exportData"],
+      children: ["unifiedReturnObject","unifiedException","validation","database","uploadFile","importData","exportData","login"],
     },
   ],
   "/convention":[
